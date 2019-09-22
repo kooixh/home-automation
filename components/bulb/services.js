a7686bdddee4d28d53941447e3c6e3a1167e71ec
Kooi Xiu Hong <kooi@Koois-MacBook-Pro.local>
2019-09-22 11:14:02 +0800

    Create endpoint to get device and add error handling for 404

--- components/bulb/services.js
+++ components/bulb/services.js
@@ -21,18 +21,33 @@ module.exports = function(){
  * @returns {Promise<Error>}
  */
 async function getDeviceByIP(host) {
-
-    let bulb = await client.getDevice({host: host});
-    return bulb;
-
+    try {
+        let bulb = await client.getDevice({host: host});
+        return bulb;
+    } catch (e) {
+        return null;
+    }
 }
 
-
+/**
+ *
+ *
+ *
+ * @param bulb
+ * @returns {Promise<{message: string, status: string}>}
+ */
 async function turnBulbOn(bulb) {
     await bulb.setPowerState(true);
     return {'message': bulb.alias+' switched on', status: 'success'};
 }
 
+/**
+ *
+ *
+ *
+ * @param bulb
+ * @returns {Promise<{message: string, status: string}>}
+ */
 async function turnBulbOff(bulb) {
     await bulb.setPowerState(false);
     return {'message': bulb.alias+' switched off', status: 'success'};
--- components/devices/routes.js
+++ components/devices/routes.js
@@ -4,5 +4,6 @@ let router = express.Router();
 let devCtrl = require('./controller');
 
 router.get('/list', devCtrl.listAllDevices);
+router.get('/ip/:host', devCtrl.getDeviceByHost);
 
 module.exports = router;

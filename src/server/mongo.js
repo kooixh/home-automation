1f23e5ad9748eb7d3e128d48b666b674f6dff5c1
Xiu Hong <xiuhong.kooi977055@gmail.com>
2020-05-26 11:07:03 +0800

    Make connect as an export function in mongo.js

--- src/server/mongo.js
+++ src/server/mongo.js
@@ -3,7 +3,10 @@ const config = require('config');
 let MongoClient = require('mongodb').MongoClient;
 let url = config.mongoDB.url;
 
+function connect(callback) {
+    MongoClient.connect(url, callback);
+}
+
 module.exports = {
-    MongoClient : MongoClient,
-    mongoUrl : url
+    mongoConnect: connect
 };

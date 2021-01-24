9a4230f22df4eccdecc29b1f57d11d0f616e7fea
Xiu Hong <xiuhong.kooi977055@gmail.com>
2021-01-24 09:27:43 +0000

    Add basic test case

---
+++ tests/utils/colour_utils_tests.js
@@ -0,0 +1,33 @@
+'use strict';
+global.mocha = require('mocha');
+
+let expect = require('chai').expect;
+
+let colourUtils = require('../../src/common/utils/colour.utils');
+
+describe('isHEXColour function', () => {
+    describe('when given a valid HEX Code', () => {
+        it('should return a correct true', function () {
+            const HEX = 'AAB4CD';
+
+            expect(colourUtils.isHEXColour(HEX)).to.be.eql(true);
+        });
+    });
+
+    describe('when given an invalid HEX Code', () => {
+        it('should return a correct false', function () {
+            const HEX = 'A10CB4CD';
+
+            expect(colourUtils.isHEXColour(HEX)).to.be.eql(false);
+        });
+    });
+
+    describe('when given empty', () => {
+        it('should return a correct false', function () {
+            const HEX = '';
+
+            expect(colourUtils.isHEXColour(HEX)).to.be.eql(false);
+        });
+    });
+
+});
